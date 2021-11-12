958a6be718d261a7d700069dc3345e7dfb5267f1
JH <taerg89@gmail.com>
2021-11-13 01:34:17 +0900

    Implementation

--- AlgorithmsStudyProject/AlgorithmsStudyProject/Stack.swift
+++ AlgorithmsStudyProject/AlgorithmsStudyProject/Stack.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+public struct Stack<Element> {
+    
+    private var storage: [Element] = []
+    
+    public init() { }
+}
+
+extension Stack: CustomStringConvertible {
+    
+    /**
+     storage.map { "\($0)" } 을 통해 아이템을 문자열로 매핑하는 배열 생성
+     reversed()를 사용하여 이전 배열을 반전시키는 새 배열 생성
+     joined(separator: )을 사용하여 배열을 문자열로 평평하게 함
+     "\n"을 사용하여 배열의 아이템을 분리
+     */
+    public var description: String {
+    """
+    ----top----
+    \(storage.map { "\($0)" }.reversed().joined(separator: "\n"))
+    -----------
+    """
+    }
+}
