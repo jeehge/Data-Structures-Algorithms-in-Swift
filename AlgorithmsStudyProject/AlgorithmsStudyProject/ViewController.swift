752dbff5783918e3c24ee4ae717d66a28f1d88e3
JH <taerg89@gmail.com>
2021-11-13 02:14:56 +0900

    Add Stack test code

--- AlgorithmsStudyProject/AlgorithmsStudyProject/ViewController.swift
+++ AlgorithmsStudyProject/AlgorithmsStudyProject/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        testStack()
     }
-
-
+    
+    func testStack() {
+        example(of: "using a stack") {
+            var stack = Stack<Int>()
+            stack.push(1)
+            stack.push(2)
+            stack.push(3)
+            stack.push(4)
+            
+            print(stack)
+            
+            if let poppedElement = stack.pop() {
+                assert(4 == poppedElement)
+                print("Popped: \(poppedElement)")
+            }
+        }
+    }
 }
 
